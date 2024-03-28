4dbf8285c4191cd58b89c71c1b7e4e658abc0d2f
SeokBeom Lee <91014308+stoneTiger0912@users.noreply.github.com>
2024-03-28 09:58:41 +0900

    [Gold IV] Title: 가르침, Time: 344 ms, Memory: 11956 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -10,88 +10,78 @@ import java.util.StringTokenizer;
  *antarctica
  *
  *a, n, t, i, c는 이미 존재
+ *31232kb 1000ms
+ *비트마스킹을 활용하면 1/10로 줄일 수 있음
+ *answer = 1<<a, n, t, i, c로 하교 비교시에 &비교하면 빠를듯
  */
 public class Main {
-	
-	static boolean[][] check;
-	
+
+	static int[] inputList;
+
 	static int n, k;
-	
-	static int[] isSelect;
-	
-	static int[] isNeed = {'a','n','t', 'i', 'c'};
-	
+
+	//	static int[] isSelect;
+
+
+
+	static int flag;
+
 	static int maxValue = 0;
-	
-	static void comb(int cnt, int idx) {
-		
+
+	static void comb(int cnt, int idx, int isSelect) {
+
 		if(cnt==k) {
-			isCheck();
+			if((flag & isSelect) == flag) {
+				isCheck(isSelect);
+			}
 			return;
 		}
-		
+
 		for(int i=idx; i<26;i++) {
-			isSelect[cnt] = i;
-			comb(cnt+1, i+1);
+			comb(cnt+1, i+1, isSelect|(1<<i));
 		}
-		
+
 	}
-	
-	static void isCheck() {
-		int flag = 0;
-		for(int i=0; i<isSelect.length;i++) {
-			for(int j=0; j<isNeed.length;j++) {
-				if(isSelect[i] == (isNeed[j]-'a')) {
-					flag++;
-				}
+
+	static void isCheck(int num) {
+		int cnt = 0;
+		for(int i=0; i<n;i++) {
+//			int check = (inputList[i] & num);
+//			if((inputList[i] & check) == check) {
+//				cnt++;
+//			}
+			if((inputList[i] & num)==inputList[i] ) {
+				cnt++;
 			}
+			
 		}
-		if(flag!=5) {
-			return;
-		}
-		
-		int result = 0;
-		
-		boolean[] ans = new boolean[26];
-		
-		for(int i=0; i<k;i++) {
-//			System.out.print(isSelect[i]+" ");
-			ans[isSelect[i]] = true;
-		}
-		
-		A: for(int i=0; i<n;i++) {
-			for(int j=0; j<26;j++) {
-				if(check[i][j]) {
-					if(!ans[j]) continue A;
-				}
-			}
-			result++;
-		}
-//		System.out.println(result);
-		
-		maxValue = Math.max(maxValue, result);
-		
+		maxValue = Math.max(maxValue, cnt);
 	}
-	
-	
+
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		
+
+
 		n = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
-		
 
-		check = new boolean[n][26];
-		
-		isSelect = new int[k];
-		
+
+		inputList = new int[n];
+
+		int[] isNeed = {'a','n','t', 'i', 'c'};
+		for(int i=0; i<5;i++) {
+			flag |= 1<<(isNeed[i] - 'a');
+		}
+
 		for(int i=0; i<n;i++) {
 			String tmp = br.readLine();
 			for(int j=0; j<tmp.length();j++) {
 				int idx = tmp.charAt(j) - 'a';
-				check[i][idx] = true;
+				if((inputList[i] & (1<<idx))==0) {
+					inputList[i] |= 1<<idx;
+				}
 			}
 		}
 
@@ -99,19 +89,9 @@ public class Main {
 			System.out.println(0);
 			return;
 		}
-//		print();
-		comb(0, 0);
+		comb(0, 0, 0);
 		System.out.println(maxValue);
-		
-		
-	}
-	
-	static void print() {
-		for(int r=0; r<n;r++) {
-			for(int c=0; c<26;c++) {
-				System.out.print(check[r][c]? (1+" "):(0+" "));
-			}
-			System.out.println();
-		}
+
+
 	}
 }
