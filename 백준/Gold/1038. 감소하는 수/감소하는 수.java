6d34dc4b5d2dc0f4f8ada8039b09e471834f3d61
SeokBeom Lee <91014308+stoneTiger0912@users.noreply.github.com>
2024-05-06 19:27:26 +0900

    [Gold V] Title: 감소하는 수, Time: 120 ms, Memory: 12984 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	
+	static int N;
+	
+	static List<Long> list = new ArrayList<>();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		
+		
+		if(N<=10) System.out.println(N);
+		//이게 왜 1022?
+		else {
+			//0번째 자리가 i인 경우
+			for(int i=0; i<10;i++) {
+				back(i, 1);
+			}
+			Collections.sort(list);
+			if(list.size()<=N) System.out.println(-1);
+			else System.out.println(list.get(N));
+		}
+	}
+	
+	//수와 자릿수
+	static void back(long num, int idx) {
+		
+		//자릿수가 10이 넘으면 987654321 보다 큰 건 없음
+		if(idx>10) return;
+		
+		//리스트 넣기
+		list.add(num);
+		
+		//현재 수에 10을 곱하고 마지막자리에 숫자를 넣기
+		for(int i=0; i<num % 10;i++) {
+			back((num*10)+i, idx+1);
+		}
+	}
+}
