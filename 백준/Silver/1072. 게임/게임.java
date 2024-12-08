b729d7b6a0709d323f481669a4748a06fdb145f7
SeokBeom Lee <91014308+stoneTiger0912@users.noreply.github.com>
2024-12-08 23:13:10 +0900

    [Silver III] Title: 게임, Time: 68 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * X 와 Y가 같으면 즉 승률이 100이므로 더이상 바뀔 수 없으므로 -1 출력
+ * 그외에 먼저 승률을 구하고 +1을 하고 나서 횟수를 빼면 됨
+ */
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long X = Integer.parseInt(st.nextToken());
+		long Y = Integer.parseInt(st.nextToken());
+		
+		//승률 소수점 이하는 버림
+		int z = (int) Math.floor(((double)(Y  * 100) / X));
+		
+		if(z>=99) {
+			System.out.println(-1);
+		}
+		else {
+			double a = Math.ceil((double)(100*Y - (z+1)*X) / (z-99));
+			
+			int res = (int) a;
+			
+			System.out.println(res);
+		}
+		
+	}
+}
