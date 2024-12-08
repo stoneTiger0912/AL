c93f9e7c64e14c774605bc3583e91408c2200eed
SeokBeom Lee <91014308+stoneTiger0912@users.noreply.github.com>
2024-12-08 23:50:53 +0900

    [Silver III] Title: 게임, Time: 64 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.java
+++ 백준/Silver/1072. 게임/게임.java
@@ -9,28 +9,53 @@ import java.util.StringTokenizer;
  * 11524KB 68ms
  */
 public class Main {
+	
+	static long X;
+	static long Y;
+	static int z;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		long X = Integer.parseInt(st.nextToken());
-		long Y = Integer.parseInt(st.nextToken());
+		X = Integer.parseInt(st.nextToken());
+		Y = Integer.parseInt(st.nextToken());
 		
 		//승률 소수점 이하는 버림
 		//여기서 100을 나중에 곱하면 오차가 발생?
-		int z = (int) Math.floor(((1.0 * Y  * 100) / X));
-//		int z = (int) Math.floor(((1.0 * Y) / X) * 100);
+		z = (int) Math.floor(((1.0 * Y  * 100) / X));
 		
-		if(z>=99) {
+		if(z >=99) {
 			System.out.println(-1);
 		}
 		else {
-			double a = Math.ceil((double)(100*Y - (z+1)*X) / (z-99));
-			
-			int res = (int) a;
-			
+			int res = binarySearch();
 			System.out.println(res);
 		}
 		
+	}
+	
+	private static int binarySearch() {
+		
+		int start = 0;
+		int end = 1_000_000_000;
+		
+		int res = 0;
+		
+		while(start<=end) {
+			int mid = (start + end) / 2;
+			
+			int next = (int) Math.floor(((1.0 * (mid+Y)  * 100) / (X+mid)));
+			
+			if(next==z) {
+				start = mid+1;
+			}
+			else {
+				end = mid-1;
+				res = mid;
+			}
+		}
+		
+		return res;
 	}
 }
