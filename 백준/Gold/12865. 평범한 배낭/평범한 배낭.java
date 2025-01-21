46c57c0c8715e7ef665d6497067518e23c4ee0e7
SeokBeom Lee <91014308+stoneTiger0912@users.noreply.github.com>
2025-01-21 19:57:53 +0900

    [Gold V] Title: 평범한 배낭, Time: 140 ms, Memory: 51336 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -3,47 +3,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
-/**
- * 1. 무게 보다 낮은 경우
- * 2. 무게 보다 높은 경우
- */
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		
-		int[][] inputList = new int[N][2];
+		int[][] array = new int[N+1][2];
 		
-		
-		for(int i=0; i<N;i++) {
+		for(int i=1; i<=N;i++) {
 			st = new StringTokenizer(br.readLine());
-
-			//0은 무게 1은 V벨류
-			inputList[i][0] = Integer.parseInt(st.nextToken());
-			inputList[i][1] = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			
+			array[i][0] = w;
+			array[i][1] = v;
 		}
 		
-		int[] pre = new int[K+1];
-		int[] next = new int[K+1];
+		//N번째를 넣을 경우 M+1이 최대인 가방을 채우는 경우
+		int[][] dp = new int[N+1][M+1];
 		
-		
-		for(int i=0; i<N;i++) {
-			int w = inputList[i][0];
-			for(int j=0; j<K+1;j++) {
-				if(inputList[i][0]> j) continue;
-				
-				next[j] = Math.max(pre[j], pre[j-w]+inputList[i][1]);
+		for(int i=1;i<=N;i++) {
+			for(int j=1; j<=M;j++) {
+				if(array[i][0] > j) {
+					dp[i][j] = dp[i-1][j];
+				}
+				else {
+					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-array[i][0]] + array[i][1]);
+				}
 			}
-//			for(int j=0; j<K+1;j++) {
-//				System.out.print(next[j]+" ");
-//			}
-//			System.out.println();
-			System.arraycopy(next, 0, pre, 0, K+1);
 		}
-		System.out.println(next[K]);
+		
+		System.out.println(dp[N][M]);
+		
 	}
 }
