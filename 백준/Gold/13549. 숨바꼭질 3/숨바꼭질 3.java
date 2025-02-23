edd9b9b6423668ad9c90f46f335a277f1f2f9309
SeokBeom Lee <91014308+stoneTiger0912@users.noreply.github.com>
2025-02-23 20:32:14 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 192 ms, Memory: 20304 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,78 +1,85 @@
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
-
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public class Main {
-	static final int MAX = 100_001;
-
-	static int start, end;
-
-	static boolean[] visited;
-
-	static class Node {
-		int idx;
+	
+	static class Node implements Comparable<Node>{
+		int num;
 		int time;
-
-		Node(int idx, int time) {
-			this.idx = idx;
+		
+		Node(int num, int time) {
+			this.num = num;
 			this.time = time;
 		}
-
-
-	}
-
-	static void Dijstra() {
-		PriorityQueue<Node> pq = new PriorityQueue<>(
-				(o1, o2) -> o1.time-o2.time == 0 ? o1.idx - o2.idx: o1.time-o2.time
-				);
-		pq.offer(new Node(start, 0));
-		visited[start] = true;
-		while(!pq.isEmpty()) {
-			Node node = pq.poll();
-			//System.out.println(node.idx+" "+node.time);
-			int idx = node.idx;
-			int time = node.time;
-			
-			if(idx==end) {
-				System.out.println(time);
-				return;
-			}
-
-			if(idx*2 <=MAX && !visited[idx*2]) {
-				pq.offer(new Node(idx*2, time));
-				visited[idx*2] = true;
-			}
-
-			if(idx+1 <= MAX &&!visited[idx+1]) {
-				pq.offer(new Node(idx+1, time+1));
-				visited[idx+1] = true;
-				
-			}
-
-			if(idx-1>=0 && !visited[idx-1]) {
-				pq.offer(new Node(idx-1, time+1));
-				visited[idx-1] = true;
-			}
-
-
+		
+		public int compareTo(Node n) {
+			return Integer.compare(this.time, n.time);
 		}
 	}
-
-
+	
+	static int BFS(int start, int end) {
+		
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		
+		int[] visited = new int[200_001];
+		
+		Arrays.fill(visited, Integer.MAX_VALUE);
+		
+		pq.offer(new Node(start, 0));
+		
+		
+		while(!pq.isEmpty()) {
+			Node n = pq.poll();
+			
+			int idx = n.num;
+			int time = n.time;
+			
+			if(idx==end) {
+				return time;
+			}
+			
+			for(int i=-1; i<2;i++) {
+				int nextIdx = idx;
+				int nextTime = time;
+				if(i==0) {
+					nextIdx += idx;
+				}
+				else {
+					nextIdx += i;
+					nextTime += 1;
+				}
+				
+				if(nextIdx >= 200_001 ||nextIdx < 0) continue;
+				
+				if(visited[nextIdx] > nextTime) {
+					visited[nextIdx] = nextTime;
+					pq.offer(new Node(nextIdx, nextTime));
+				}
+				
+				
+			}
+			
+			
+		}
+		return 0;
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		start = Integer.parseInt(st.nextToken());
-		end = Integer.parseInt(st.nextToken());
-
-		visited = new boolean[MAX+1];
-
-		Dijstra();
-
-
+		
+		int start = Integer.parseInt(st.nextToken());
+		int end = Integer.parseInt(st.nextToken());
+		
+		int result = BFS(start, end);
+		
+		System.out.println(result);
+		
 	}
 }
