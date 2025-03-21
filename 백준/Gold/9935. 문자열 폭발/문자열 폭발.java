c51b5ac91d5f995a16f2511e08609b1e7bb052de
SeokBeom Lee <91014308+stoneTiger0912@users.noreply.github.com>
2025-03-21 21:26:39 +0900

    [Gold IV] Title: 문자열 폭발, Time: 980 ms, Memory: 37276 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -4,39 +4,41 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		String text = st.nextToken();
-		st = new StringTokenizer(br.readLine());
-		String check = st.nextToken();
 		
-		int textLen = text.length();
-		int checkLen = check.length();
+		String input = br.readLine();
+		
+		String word = br.readLine();
 		
 		Stack<Character> stack = new Stack<>();
-
-		for(int i=0; i<textLen;i++) {
-			int cnt = 0;
-			stack.push(text.charAt(i));
+		
+		int inputLen = input.length();
+		int wordLen = word.length();
+		
+		for(int i=0; i<inputLen;i++) {
+			char c = input.charAt(i);
 			
-			if(stack.size()>=checkLen) {
-				
-				for(int j=0; j<checkLen;j++) {
-					if(stack.get(stack.size()-checkLen + j) == check.charAt(j)) {
+			stack.push(c);
+			
+			if(stack.size() >= wordLen) {
+				int cnt = 0;
+				for(int j=0; j<wordLen;j++) {
+					if(stack.get(stack.size() - wordLen + j) == word.charAt(j)) {
 						cnt++;
 					}
-					
-					if(cnt==checkLen) {
-						for(int k=0; k<checkLen;k++) {
-							stack.pop();
-						}
+				}
+				
+				if(cnt==wordLen) {
+					for(int j=0; j<wordLen;j++) {
+						stack.pop();
 					}
 				}
+				
 			}
+			
 		}
 		
 		if(stack.isEmpty()) {
@@ -44,10 +46,13 @@ public class Main {
 		}
 		else {
 			StringBuilder sb = new StringBuilder();
-			for(char ch: stack) {
-				sb.append(ch);
+			
+			for(char c:stack) {
+				sb.append(c);
 			}
+			
 			System.out.println(sb);
 		}
+		
 	}
 }
