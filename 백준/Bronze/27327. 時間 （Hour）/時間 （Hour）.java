eb88f3304b6371b5cf4bcf7bcfbf4f82e619a091
SeokBeom Lee <91014308+stoneTiger0912@users.noreply.github.com>
2025-04-01 23:54:32 +0900

    [Bronze V] Title: 時間 (Hour), Time: 92 ms, Memory: 12832 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int h = sc.nextInt();
+		System.out.println(h * 24);
+		sc.close();
+	}
+}
