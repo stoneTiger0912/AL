90d4876424b24588a4fd50d3417849cd3cfad259
SeokBeom Lee <91014308+stoneTiger0912@users.noreply.github.com>
2025-04-11 00:18:16 +0900

    [Gold I] Title: 낚시왕, Time: 476 ms, Memory: 32708 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.java
+++ 백준/Gold/17143. 낚시왕/낚시왕.java
@@ -1,156 +1,179 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static int R, C, M;
-
-	static class Shark implements Comparable<Shark> {
+	
+	static final int UP = 0, DOWN = 3, LEFT = 1, RIGHT = 2;
+	
+	static int R, C;
+	static Shark[][] graph;
+	//상 좌 우 하
+	static int[] dr = {-1, 0, 0, 1};
+	static int[] dc = {0, -1, 1, 0};
+	
+	
+	static class Shark {
 		int r;
 		int c;
 		int speed;
-		int d;
+		int dir;
 		int size;
-
-
-		public Shark() {
-		}
-
-		public Shark(int r, int c, int speed, int d, int size) {
+		
+		Shark(int r, int c, int speed, int dir, int size) {
 			this.r = r;
 			this.c = c;
 			this.speed = speed;
-			this.d = d;
+			this.dir = dir;
 			this.size = size;
 		}
-
-		@Override
-		public int compareTo(Shark o) {
-			return Integer.compare(this.size, o.size)*-1;
-		}
-
-		public void move() {
-
-			for(int t=0; t<speed;t++) {
-				int nr = this.r + dr[d];
-				int nc = this.c + dc[d];
-
-				if(nr==0 || nr == R+1 || nc ==0 || nc == C+1) {
-					this.d = 3 - this.d;
-					nr = this.r + dr[d];
-					nc = this.c + dc[d];
+		
+		void move() {
+			int cnt = this.speed;
+			
+			int nr = 0;
+			int nc = 0;
+			
+			while(--cnt >= 0) {
+				switch (this.dir) {
+				case UP:
+					
+					nr = r + dr[this.dir];
+					if(nr < 0) {
+						this.dir = DOWN;
+						nr = r + dr[this.dir];
+					}
+					
+					this.r = nr;
+					
+					break;
+				case DOWN:
+					nr = r + dr[this.dir];
+					if(nr >=R) {
+						this.dir = UP;
+						nr = r + dr[this.dir];
+					}
+					
+					this.r = nr;
+					break;
+				case LEFT:
+					nc = c + dc[this.dir];
+					if(nc < 0) {
+						this.dir = RIGHT;
+						nc = c + dc[this.dir];
+					}
+					
+					this.c = nc;
+					break;
+				case RIGHT:
+					nc = c + dc[this.dir];
+					if(nc >= C) {
+						this.dir = LEFT;
+						nc = c + dc[this.dir];
+					}
+					
+					this.c = nc;
+					break;
 				}
-
-				this.r = nr;
-				this.c = nc;
 			}
 		}
-
-		@Override
-		public String toString() {
-			return "Shark [r=" + r + ", c=" + c + ", speed=" + speed + ", d=" + d + ", size=" + size + "]";
-		}
-		
-		
-
-
 	}
-
-	static int[] dr = {-1, 0, 0, 1};
-	static int[] dc = {0, -1, 1, 0};
-
-	static List<Shark> list;
-
+	
 	static void move() {
-
-		Shark[][] sharks = new Shark[R+1][C+1];
-
-		Collections.sort(list);
-		int size = list.size();
-		while(--size>=0) {
-			Shark shark = list.remove(0);
-			shark.move();
-
-			if(sharks[shark.r][shark.c]==null) {
-				sharks[shark.r][shark.c] = shark;
-				list.add(shark);
+		Queue<Shark> queue = new ArrayDeque<>();
+		
+		for(int r=0; r<R;r++) {
+			for(int c=0; c<C;c++) {
+				if(graph[r][c]!=null) {
+					queue.offer(graph[r][c]);
+					graph[r][c] = null;
+				}
+				
 			}
-
+		}
+		
+		while(!queue.isEmpty()) {
+			Shark shark = queue.poll();
+			
+			shark.move();
+			
+			int r = shark.r;
+			int c = shark.c;
+			if(graph[r][c]==null) {
+				graph[r][c] = shark;
+			}
+			else {
+				if(graph[r][c].size < shark.size) {
+					graph[r][c] = shark;
+				}
+			}
 		}
 	}
-
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		list = new ArrayList<>();
-
-		for(int i=0; i<M;i++) {
+		int m = Integer.parseInt(st.nextToken());
+		
+		graph = new Shark[R][C];
+		
+		for(int i=0; i<m;i++) {
 			st = new StringTokenizer(br.readLine());
-			int r = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
+			int r = Integer.parseInt(st.nextToken())-1;
+			int c = Integer.parseInt(st.nextToken())-1;
 			int speed = Integer.parseInt(st.nextToken());
-			int d = Integer.parseInt(st.nextToken());
+			int dir = Integer.parseInt(st.nextToken());
 			int size = Integer.parseInt(st.nextToken());
-
-			//위 아래 오른쪽 왼쪽
-			//위 왼쪽 오른쪽 아래
-			if(d==1) d = 0;
-			else if(d==2) d = 3;
-			else if(d==3) d = 2;
-			else if(d==4) d = 1;
 			
-			//상하인 경우
-			if(d==0 || d==3) {
-				speed %= ((R-1)*2);
+			switch (dir) {
+			case 1:
+				dir = 0;
+				break;
+			case 2:
+				dir = 3;
+				break;
+			case 3:
+				dir = 2;
+				break;
+			case 4:
+				dir = 1;
+				break;
+			}
+			
+			if(dir==UP || dir == DOWN) {
+				speed %= (R-1) * 2;
 			}
 			else {
-				speed %= ((C-1)*2);
+				speed %=(C-1) * 2;
 			}
-
-			Shark shark = new Shark(r, c, speed, d, size);
-			list.add(shark);
-		}
-
-		int result = 0;
-
-		for(int c=1; c<=C;c++) {
-			int idx = -1;
-			for(int i=0; i<list.size();i++) {
-				//같은 줄에 있으면
-				Shark shark = list.get(i);
-				if(shark.c==c) {
-					if(idx==-1) idx = i;
-					else {
-						if(list.get(idx).r < shark.r) continue;
-						idx = i;
-					}
-				}
-			}
-			//제거
-			if(idx!=-1) {
-				Shark removeShark = list.remove(idx);
-				result += removeShark.size;
-			}
-			//움직임
-			move();
 			
-//			for(Shark s:list) {
-//				System.out.println(s);
-//			}
-//			System.out.println(result);
+			Shark shark = new Shark(r, c, speed, dir, size);
+			
+			graph[r][c] = shark;
+			
 		}
 		
-//		for(Shark s:list) {
-//			System.out.println(s);
-//		}
+		int result = 0;
+		
+		for(int c=0; c<C;c++) {
+			for(int r=0; r<R;r++) {
+				if(graph[r][c]!=null) {
+					result += graph[r][c].size;
+					graph[r][c] = null;
+					break;
+				}
+			}
+			
+			//모든 상어 움직임
+			move();
+			
+		}
 		
 		System.out.println(result);
 		
