c510edb8484abae619cb03b98aa3e1965f9dbc6c
SeokBeom Lee <91014308+stoneTiger0912@users.noreply.github.com>
2025-05-06 23:47:45 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 88 ms, Memory: 12752 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+
+        if(s.equals("SONGDO")){
+            System.out.println("HIGHSCHOOL");
+        }else if(s.equals("CODE")){
+            System.out.println("MASTER");
+        }else if(s.equals("2023")){
+            System.out.println("0611");
+        }else if(s.equals("ALGORITHM")){
+            System.out.println("CONTEST");
+        }
+
+    }
+}
