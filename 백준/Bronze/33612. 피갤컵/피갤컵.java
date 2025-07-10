45968e866a6fed6240bab3b568aa2755796a9e37
SeokBeom Lee <91014308+stoneTiger0912@users.noreply.github.com>
2025-07-10 22:15:10 +0900

    [Bronze V] Title: 피갤컵, Time: 92 ms, Memory: 12828 KB -BaekjoonHub

---
+++ 백준/Bronze/33612. 피갤컵/피갤컵.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int year = 2024;
+        int month = 8;
+
+        int N = sc.nextInt();
+
+        if(N == 1) {
+            System.out.println(year + " " + month);
+        } else {
+            month += (7 * (N-1));
+
+            if(month % 12 != 0) {
+                year += month / 12;
+                month = month % 12;
+            } else {
+                year += month / 12 - 1;
+                month = 12;
+            }
+
+
+            System.out.println(year + " " + month);
+        }
+    }
+}
