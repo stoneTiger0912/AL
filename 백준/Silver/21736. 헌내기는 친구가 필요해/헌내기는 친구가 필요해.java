bc24534d8206005af9e36b1f2857c71349af92ba
SeokBeom Lee <91014308+stoneTiger0912@users.noreply.github.com>
2025-09-22 10:58:54 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 280 ms, Memory: 26580 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -6,93 +6,105 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
+	
 	static int R, C;
-	static char[][] graph;
-	static boolean[][] visited;
-	static int startR, startC;
-	static int cnt;
-
-	static int[] dr = {-1, 1, 0, 0};
-	static int[] dc = {0, 0, -1, 1};
-
-	static void BFS() {
-		Queue<Node> queue = new ArrayDeque<>();
-		queue.offer(new Node(startR, startC));
-		visited[startR][startC] = true;
-
-		while(!queue.isEmpty()) {
-			Node node = queue.poll();
-			int r = node.r;
-			int c = node.c;
-
-			for(int d=0; d<4;d++) {
-				int nr = r + dr[d];
-				int nc = c + dc[d];
-//				System.out.println(nr+" "+nc);
-
-				if(nr < 0 || nr >=R || nc < 0 || nc >=C ||visited[nr][nc]||graph[nr][nc]=='X') continue;
-
-				if(graph[nr][nc]=='P') cnt++;
-
-				queue.offer(new Node(nr, nc));
-				visited[nr][nc] = true;
-			}
-		}
-
-	}
-
+	static int[][] graph;
+	
 	static class Node {
 		int r;
 		int c;
-
+		
 		Node(int r, int c) {
 			this.r = r;
 			this.c = c;
 		}
-
-		@Override
-		public String toString() {
-			return "Node [r=" + r + ", c=" + c + "]";
-		}
-
-
 	}
-
+	
+	static int result = 0;
+	
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	
+	static void BFS(int startR, int startC) {
+		boolean[][] visited = new boolean[R][C];
+		Queue<Node> queue = new ArrayDeque<>();
+		
+		queue.offer(new Node(startR, startC));
+		visited[startR][startC] = true;
+		
+		while(!queue.isEmpty()) {
+			Node n = queue.poll();
+			
+			for(int d=0; d<4;d++) {
+				int nr = n.r + dr[d];
+				int nc = n.c + dc[d];
+				
+				if(nr < 0 || nr >=R  || nc < 0 || nc >= C || graph[nr][nc]==1 || visited[nr][nc]) continue;
+				
+				if(graph[nr][nc]==2) result++;
+				
+				queue.offer(new Node(nr, nc));
+				visited[nr][nc] = true;
+				
+			}
+			
+		}
+		
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
-
-
-		graph = new char[R][C];
-		visited = new boolean[R][C];
-
+		
+		int startR = 0, startC = 0;
+		
+		graph = new int[R][C];
+		
 		for(int r=0; r<R;r++) {
+			
 			String tmp = br.readLine();
-
+			
 			for(int c=0; c<C;c++) {
-				graph[r][c] = tmp.charAt(c);
-			}
-
-		}
-
-		A: for(int r=0; r<R;r++) {
-			for(int c=0; c<C;c++) {
-				if(graph[r][c]=='I') {
+				
+				switch (tmp.charAt(c)) {
+				case 'O':
+					graph[r][c] = 0;
+					break;
+				case 'X':
+					graph[r][c] = 1;
+					break;
+				case 'I':
 					startR = r;
 					startC = c;
-					graph[r][c] = 'O';
-					break A;
+					graph[r][c] = 0;
+					
+					break;
+				case 'P':
+					
+					graph[r][c] = 2;
+					
+					break;
 				}
 			}
 		}
-//		System.out.println(startR +" "+ startC);
 		
-		BFS();
-		System.out.println(cnt==0 ? "TT": cnt);
-
+//		print();
+		BFS(startR, startC);
+		System.out.println(result==0 ? "TT" : result);
+		
+		
+	}
+	
+	static void print() {
+		for(int r=0; r<R;r++) {
+			for(int c=0; c<C;c++) {
+				System.out.print(graph[r][c]+" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
 	}
 }
